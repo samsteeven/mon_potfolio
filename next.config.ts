4e87f836f62d8cb62e045e2ca9370a750b8f366e
samsteeven <samendjiaha@gmail.com>
2026-07-01 02:14:52 +0100

    allow l'IP 192.168.56.1 du serveur de dev

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import { createMDX } from "fumadocs-mdx/next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["192.168.56.1"],
   turbopack: {
     root: __dirname,
   },
